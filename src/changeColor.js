e1f84dc0d55b56e73b83cf5c282b8d4245a5591b
Daniel Gomez <danielgomez@MacBook-Pro-de-Daniel.local>
2017-11-01 16:29:19 -0500

    se creo el componente ChangeColor

---
+++ src/changeColor.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default class ChangeColor extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+
+            tamanoLetra: 5
+            
+        }
+        this.aumentarTamano = this.aumentarTamano.bind(this);
+    }
+
+
+    aumentarTamano (){
+        const tamActual = this.state.tamanoLetra;
+
+        this.setState ({tamanoLetra:tamActual +10});
+
+    }
+
+        render () {
+    return (
+        <div>
+
+        <h1 style={{fontSize: this.state.tamanoLetra}}>TITULO DE LA PAGINA  </h1>
+
+        <button onClick={this.aumentarTamano}>Aumentar tamaño</button>
+
+
+            </div>
+
+    );
+
+   }
+
+}
--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { render } from 'react-dom'
 import Hello from './Hello'
 import ImageClick from './ImageClick'
+import ChangeColor from './changeColor'
 
 const fn = function() {
   alert('mi funcion');
 };
 
+
 const app = (
   <div>
     <Hello />
     <ImageClick onImgClick={fn} /> 
+    <ChangeColor />
   </div>
 );
 
